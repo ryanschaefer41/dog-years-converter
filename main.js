9e3eaed49249beff922d5a43e22298d923dbe6b0
Ryan Schaefer <23317410+ryanschaefer41@users.noreply.github.com>
2021-01-25 14:47:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ let myAge = 28; // set my age as variable
 let earlyYears = 2; // will be used to calculate the first two years of dog's life
 earlyYears *= 10.5;
 
-let laterYears = myAge - 2; // we already accounted for the first two years above
-laterYears *= 4; // calculate dig years for later human years, and reassign to the laterYears variable
+let laterYears = myAge - 2; // already accounted for the first two years above
+laterYears *= 4; // calculate dog years for later human years, and reassign to the laterYears variable
 
 // console.log(earlyYears);
 // console.log(laterYears);
